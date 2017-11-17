fe7f6ea2d2e8c943f73e613cf67baaa6255a5093
xueqingxiao <xueqingxiao@gmail.com>
2017-11-17 17:37:32 +0800

    init

--- bin/start.sh
+++ bin/start.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
-CHROME_ARGS="--disable-gpu --headless --no-sandbox --remote-debugging-address=$DEBUG_ADDRESS --remote-debugging-port=$DEBUG_PORT --user-data-dir=/data"
+CHROME_OPTS="--disable-gpu --headless --no-sandbox --remote-debugging-address=0.0.0.0 --remote-debugging-port=9222 --user-data-dir=/home/chrome"
 
-if [ -n "$CHROME_OPTS" ]; then
-  CHROME_ARGS="${CHROME_ARGS} ${CHROME_OPTS}"
+if [ -n "$CHROME_ARGS" ]; then
+  CHROME_ARGS="${CHROME_OPTS} ${CHROME_ARGS}"
 fi
 
-# Start Chrome
-sh -c "/usr/bin/google-chrome-unstable $CHROME_ARG
+sh -c "/usr/bin/google-chrome-unstable $CHROME_ARGS
