edbd464cf92d267ac0365b78e4fcfbbfb5db2a0a
xueqingxiao <xueqingxiao@gmail.com>
2017-11-20 10:15:10 +0800

    fixs: args incorrect

--- bin/start.sh
+++ bin/start.sh
@@ -3,7 +3,7 @@
 ARGS="--disable-gpu --headless --no-sandbox --remote-debugging-address=0.0.0.0 --remote-debugging-port=9222"
 
 if [ -n "$CHROME_ARGS" ]; then
-  CHROME_ARGS="${ARGS} ${CHROME_ARGS}"
+  ARGS="${ARGS} ${CHROME_ARGS}"
 fi
 
 sh -c "/usr/bin/google-chrome-stable $ARGS"
